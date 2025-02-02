87b7c2fc75a8f363f3d35ab94cf119ab76daa2c4
Toni Robres <toni.robres@scrm.lidl>
2025-02-02 20:37:28 +0100

    delete by label

--- playwright/locators/test_locate_by_label_missing.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page
-
-URL = 'https://forumhtml.azurewebsites.net/v1.0/demo'
-
-
-def test_playwright_shortcut(page: Page):
-    page.goto(URL)
-    label_text = page.get_by_label('Windows')
-    print(label_text.text_content())
