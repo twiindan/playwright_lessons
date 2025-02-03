eeefc5441eadce7307c33829768d640ce6215bed
Toni Robres <toni.robres@scrm.lidl>
2025-02-03 17:16:29 +0100

    add test to configure test_id

--- playwright/locators/test_locate_by_test_id.py
+++ playwright/locators/test_locate_by_test_id.py
@@ -1,3 +1,4 @@
+import playwright.sync_api
 from playwright.sync_api import Page
 
 URL = 'https://forumhtml.azurewebsites.net/v1.0/demo'
@@ -8,3 +9,10 @@ def test_playwright_shortcut(page: Page):
     open_window_button = page.get_by_test_id("OpenTab")
     print(open_window_button.is_visible())
 
+
+def test_set_test_id(page: Page):
+    from playwright.sync_api import sync_playwright, Playwright
+    playwright.sync_api.Selectors.set_test_id_attribute('data-testingid')
+    page.goto(URL)
+    placeholder = page.get_by_test_id("placeholder")
+    print(placeholder.is_visible())
