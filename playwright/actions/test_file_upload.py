e52e48048574d60d718a37db650f7a6af6c637fc
Toni Robres <toni.robres@scrm.lidl>
2025-02-12 11:56:13 +0100

    add file upload test

---
+++ playwright/actions/test_file_upload.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import Page, expect
+
+
+def test_file_upload(page: Page):
+    page.goto('https://letcode.in/file')
+    page.locator('.file-input')
+    with page.expect_file_chooser() as fc_info:
+        page.locator('.file-cta').click()
+    file_chooser = fc_info.value
+    file_chooser.set_files("test_file_upload.py")
+    expect(page.locator('p.label')).to_have_text('Selected File: test_file_upload.py')
