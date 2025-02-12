54767e523220801f9496e203c0d6d9e27f463840
Toni Robres <toni.robres@scrm.lidl>
2025-02-12 12:51:00 +0100

    Change path

--- playwright/actions/test_file_upload.py
+++ playwright/actions/test_file_upload.py
@@ -7,5 +7,5 @@ def test_file_upload(page: Page):
     with page.expect_file_chooser() as fc_info:
         page.locator('.file-cta').click()
     file_chooser = fc_info.value
-    file_chooser.set_files("tooling")
-    expect(page.locator('p.label')).to_have_text('Selected File: tooling')
+    file_chooser.set_files("README.md")
+    expect(page.locator('p.label')).to_have_text('Selected File: README.md')
