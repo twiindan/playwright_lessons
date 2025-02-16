49302f552ecd4f8810acc44d7320e0601c6abf43
Toni Robres <toni.robres@scrm.lidl>
2025-02-16 18:08:15 +0100

    http

--- playwright/actions/test_file_upload.py
+++ playwright/actions/test_file_upload.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Page, expect
 
 
 def test_file_upload(page: Page):
-    page.goto('https://letcode.in/file')
+    page.goto('http://letcode.in/file')
     page.locator('.file-input')
     with page.expect_file_chooser() as fc_info:
         page.locator('.file-cta').click()
