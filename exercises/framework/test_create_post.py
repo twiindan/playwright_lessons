82b773ba6d1f54e01dec027cae51569400d0538a
Toni Robres <toni.robres@scrm.lidl>
2025-02-16 18:59:26 +0100

    Create post test

---
+++ exercises/framework/test_create_post.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import Page, expect
+
+from exercises.framework.models.user import User
+from page.login_page import LoginPage
+from page.main_page import MainPage
+from page.register_page import RegisterPage
+
+
+def test_register_user(page: Page, random_user: User):
+
+    post_text = 'My second post created'
+    register_page = RegisterPage(page)
+    register_page.load()
+
+    register_page.register_user(user=random_user)
+    login_page = LoginPage(page)
+    expect(login_page.get_success_alert_locator()).to_have_text('Congratulations, you are now a registered user!')
+
+    login_page.login(user=random_user)
+    main_page = MainPage(page)
+    expect(main_page.get_logout_locator()).to_be_visible()
+    main_page.create_new_post(post_text)
+    expect(main_page.get_post_by_text(post_text)).to_be_visible()
