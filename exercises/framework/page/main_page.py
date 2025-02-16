e3ea3288e218ff3c0707b869c66c4bf4c48c03d1
Toni Robres <toni.robres@scrm.lidl>
2025-02-16 19:01:47 +0100

    add methods in main page

--- exercises/framework/page/main_page.py
+++ exercises/framework/page/main_page.py
@@ -6,6 +6,22 @@ class MainPage:
     def __init__(self, page: Page):
         self.page = page
         self.logout_locator = page.get_by_role("link", name="Logout")
+        self.post_textbox_locator = page.locator("#post")
+        self.submit_button = page.locator("#submit")
+        self.profile_locator = page.get_by_role("link", name="Profile")
 
     def get_logout_locator(self):
         return self.logout_locator
+
+    def create_new_post(self, post_text):
+        self.post_textbox_locator.fill(post_text)
+        self.submit_button.click()
+
+    def get_post_by_text(self, text):
+        return self.page.locator("td").filter(has_text=text)
+
+    def go_to_profile_page(self):
+        self.profile_locator.click()
+
+    def logout(self):
+        self.logout_locator.click()
